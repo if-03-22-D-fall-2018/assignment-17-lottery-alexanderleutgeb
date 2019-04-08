930ba4a3da89e27b89bb5cda75bf07617a89952b
Leutgeb Alexander <if170078@WS8A01.htl-leonding.ac.at>
2019-04-08 09:44:31 +0200

    tests running

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -10,3 +10,23 @@
  * Implementation of a library for analyzing lottery tips.
  *-----------------------------------------------------------------------------
 */
+#include <stdio.h>
+#include "lottery.h"
+#define UUID_LEN 40
+#define MAX_TIP_LEN 17
+#define MAX_LINE_LEN (UUID_LEN + 1 + MAX_TIP_LEN +1)
+bool init_lottery(const char* csv_file, char csv_seperator){
+ return false;
+}
+bool get_tip(int tip_number, int tip[TIP_SIZE]){
+ return false;
+}
+bool set_drawing(int drawing_numbers[TIP_SIZE]){
+ return false;
+}
+int get_tip_result(int tip_number){
+ return 0;
+}
+int get_right_tips_count(int right_digits_count){
+ return 0;
+}
